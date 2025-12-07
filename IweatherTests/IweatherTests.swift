fc6f8d7cc0282162387fa46ebb72f0b014a4e48c
henriquepablo <pablo269704@gmail.com>
2025-12-07 16:48:58 -0300

    Initial Commit

---
+++ IweatherTests/IweatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  IweatherTests.swift
+//  IweatherTests
+//
+//  Created by pablo henrique on 07/12/25.
+//
+
+import Testing
+@testable import Iweather
+
+struct IweatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
